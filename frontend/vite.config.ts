a0de8a7ccc771cbf6e13d25575eeaf4460f7117d
US11 <24100010112.uset@ltsu.ac.in>
2026-07-11 13:05:48 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,4 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  nitro: {
-    // 1. Tell Nitro NOT to render the React code dynamically
-    routeRules: {
-      '/**': { ssr: false }
-    },
-    // 2. Force Nitro to generate a blank SPA fallback file named 200.html
-    prerender: {
-      routes: ['/200.html']
-    }
-  }
 });
